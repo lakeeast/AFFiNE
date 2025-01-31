6705c4c241b29ef145305554869bde3f1a746f06
Yuanming Chen <yuanming@gmail.com>
2025-01-31 15:55:29 -0700

    Your commit message

--- packages/frontend/core/src/components/root-app-sidebar/index.tsx
+++ packages/frontend/core/src/components/root-app-sidebar/index.tsx
@@ -189,15 +189,10 @@ export const RootAppSidebar = memo((): ReactElement => {
             <span data-testid="import-modal-trigger">{t['Import']()}</span>
           </MenuItem>
           <TemplateDocEntrance />
-          <ExternalMenuLinkItem
-            href="https://affine.pro/blog?tag=Release+Note"
-            icon={<JournalIcon />}
-            label={t['com.affine.app-sidebar.learn-more']()}
-          />
         </CollapsibleSection>
       </SidebarScrollableContainer>
       <SidebarContainer>
-        {BUILD_CONFIG.isElectron ? <UpdaterButton /> : <div/>}
+        {BUILD_CONFIG.isElectron ? <UpdaterButton /> : <div />}
       </SidebarContainer>
     </AppSidebar>
   );
