04be307ee7aaad699ba3565fc97841b7dce11ee7
Yuanming Chen <yuanming@gmail.com>
2025-02-13 23:45:55 -0700

    Refactor Dicom viewer

--- packages/frontend/core/src/components/attachment-viewer/dicom-viewer.tsx
+++ packages/frontend/core/src/components/attachment-viewer/dicom-viewer.tsx
@@ -6,33 +6,36 @@ import { getAttachmentBlob } from './utils';
 export function DicomViewer({ model, ...props }: PDFViewerProps) {
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const [webServerUrl, setWebServerUrl] = useState<string | undefined>(undefined);
+  const modelRef = useRef(model);
+
+  // Update modelRef whenever model changes
+  useEffect(() => {
+    modelRef.current = model;
+  }, [model]);
 
   useEffect(() => {
     const isCloudFront = window.location.hostname.includes("docnosys.com");
-    if (isCloudFront) {
-      setWebServerUrl("https://dicom.docnosys.com/local");
-    }
-    else {
-      setWebServerUrl("http://localhost:5555/local");
-    }
+    setWebServerUrl(isCloudFront ? "https://dicom.docnosys.com/local?v=0" : "http://localhost:5555/local?v=0");
+  }, []);
 
+  useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
+      // Ignore messages from setImmediate in polyfill.js
+      if (typeof event.data === "string" && event.data.startsWith("setImmediate$")) {
+        return;
+      }
+
       if (event.data.type === 'ohifReady') {
         try {
-          const fetchedBlob = await getAttachmentBlob(model);
-          // Unzip fetchedBlob
+          const fetchedBlob = await getAttachmentBlob(modelRef.current);
           const zip = new JSZip();
           const zipFile = await zip.loadAsync(fetchedBlob as any);
-          // Prepare an array to hold the resulting Blobs
-          const blobs: Blob[] = [];
 
-          // Iterate over each file in the ZIP archive
-          for (const [filename, file] of Object.entries(zip.files)) {
-            // Extract the file as a Blob
-            const fileBlob = await file.async('blob');
-            blobs.push(fileBlob);
-          }
-          console.log('Sending blobs to iFrame:', blobs);
+          const blobs: Blob[] = await Promise.all(
+            Object.values(zip.files).map(file => file.async('blob'))
+          );
+
+          console.log(`Sending to iFrame: with ${blobs.length} blobs`);
           iframeRef?.current?.contentWindow?.postMessage(blobs, '*');
         } catch (error) {
           console.error('Error sending blob to iFrame:', error);
@@ -41,19 +44,18 @@ export function DicomViewer({ model, ...props }: PDFViewerProps) {
     };
 
     window.addEventListener('message', handleMessage);
-
     return () => {
       window.removeEventListener('message', handleMessage);
     };
-  }, [model]);
+  }, []); // Only run once when the component mounts
 
   return (
     <div style={{ display: "flex", flex: 1, flexDirection: "column", height: "100%" }}>
       <iframe
-      ref={iframeRef}
-      src={webServerUrl}
-      style={{ flex: 1, width: "100%", height: "100%", border: "none" }}
-      title="DICOM Viewer"
+        ref={iframeRef}
+        src={webServerUrl}
+        style={{ flex: 1, width: "100%", height: "100%", border: "none" }}
+        title="DICOM Viewer"
       />
     </div>
   );
